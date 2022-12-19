162bd5522a415bc3fb08eb099366701113c6ac25
Juholee11 <104490488+Juholee11@users.noreply.github.com>
2022-12-19 21:48:03 +0900

    임시.

---
+++ src/main/java/com/bitstudy/app/domian/Ex00_1_Article.java
@@ -0,0 +1,19 @@
+package com.bitstudy.app.domian;
+
+import java.time.LocalDateTime;
+
+public class Ex00_1_Article {
+    private Long id;
+    private String title; //제목
+    private String content; //본문
+    private String hashtag; //해시태그
+
+    //메타데이터(개발자가 보는 데이터를 의미
+    private LocalDateTime createdAt; //생성일시
+    private String createdBy; // 생성자
+    private LocalDateTime modifiedAt; // 수정일시
+    private String modifiedBy; // 수정자
+
+
+
+}
