4beafe7a61bafbd5e1743618c2628bc5b577cc76
Juholee11 <104490488+Juholee11@users.noreply.github.com>
2022-12-19 19:13:51 +0900

    #10 - 도메인 클래스 설계(게시글, 댓글)
    도메인 정보 설계 하였음 (Article, ArticleComment)
    
    기초적인 게시글과 댓글 표현이 가능하도록 제작
    DB와 연동을 위한 추가적인 표현은 추후 업데이트 예정

---
+++ src/main/java/com/bitstudy/app/domian/ArticleComment.java
@@ -0,0 +1,23 @@
+package com.bitstudy.app.domian;
+
+import java.time.LocalDateTime;
+
+public class ArticleComment {
+
+    private Long id;
+
+    private Article article; // Article 파일에 내용을 여기에 넣는 작업 article.id 이런식으로 가져다 쓸 수 있다
+/*  연관 관계 매핑
+    연관 관계 없이 만든다면 -> private Long articleId; 이런식으로(관계형 데이터베이스 스타일) 하면 되지만
+    현재 우리는 Article 와 ArticleComment 가 관계를 맺고 있는것을 객체 지향적으로 표현하려고 이렇게 사용한다(JPA 이용)
+ */
+
+    private String content; //본문
+    private String hashtag; //해시태그
+
+    //메타데이터(개발자가 보는 데이터를 의미
+    private LocalDateTime createdAt; //생성일시
+    private String createdBy; // 생성자
+    private LocalDateTime modifiedAt; // 수정일시
+    private String modifiedBy; // 수정자
+}
